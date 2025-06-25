52e49930f4ae339139ab1ce6aad0ffc8f651418c
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-06-25 12:05:54 +0530

    StreakForge UI coming alive! Header, Timer skeleton, and controls in place

--- StreakForge/src/App.jsx
+++ StreakForge/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Timer from "./components/Timer";
+import { FaPlay, FaRedo, FaCog } from "react-icons/fa";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="w-full h-screen ">
+      <div className="flex  justify-center items-center min-h-screen bg-gray-900">
+        <div className="w-[60vw] h-[80vh] bg-gray-700 flex flex-col  items-center rounded-[6rem]  pb-[5rem] pt-[2rem]">
+          {/* Header: Focus , Break, Rest */}
+          <header className="w-full max-w-md flex justify-around text-white  gap-10 mb-8 ">
+            <button className="flex-1 text-center py-2 font-semibold bg-gray-500 rounded hover:bg-gray-600">
+              Focus
+            </button>
+            <button className="flex-1 text-center py-2 font-semibold bg-gray-500 rounded hover:bg-gray-600">
+              Break
+            </button>
+            <button className="flex-1 text-center py-2 font-semibold bg-gray-500 rounded hover:bg-gray-600">
+              Rest
+            </button>
+          </header>
 
-export default App
+          {/* Timer Section */}
+          <main className="flex-grow text-white flex flex-col justify-center items-center">
+            <Timer />
+          </main>
+
+          {/* Bottom Controls */}
+          <footer className="w-full max-w-md flex justify-around items-center mt-8">
+            <div className="flex gap-[8rem]">
+              <FaRedo className="w-6 h-6 cursor-pointer text-gray-300" />
+              <FaPlay className="w-8 h-8 cursor-pointer text-white" />
+              <FaCog className="w-6 h-6 cursor-pointer text-gray-300" />
+            </div>
+          </footer>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
